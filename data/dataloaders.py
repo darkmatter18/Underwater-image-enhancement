e871eb81bf7f4d2dcb04fbcd9746d1bcca047d27
Arkadip <in2arkadipb13@gmail.com>
2020-10-01 13:02:35 +0530

    fix: OverflowError: cannot convert float infinity to integer

--- data/dataloaders.py
+++ data/dataloaders.py
@@ -30,7 +30,7 @@ class CustomDatasetDataLoader:
 
     def __len__(self):
         """Return the number of data in the dataset"""
-        return min(len(self.dataset), int(self.max_dataset_size))
+        return min(len(self.dataset), self.max_dataset_size)
 
     def __iter__(self):
         """Return a batch of data"""

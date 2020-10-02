7c1a820dc998d439f88bd0352479cad31eeaab01
Arkadip <in2arkadipb13@gmail.com>
2020-10-02 22:21:50 +0530

    feat: Training Loop Complete
    - implemented save_current_visuals
    - all pipelines done

--- train.py
+++ train.py
@@ -1,22 +1,46 @@
-from options.TrainOptions import TrainOptions
+import time
 from data import create_dataset
 from model.CycleGan import CycleGan
-
-import matplotlib.pyplot as plt
+from options.TrainOptions import TrainOptions
+from utils.TrainStats import TrainStats
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()
     dataset = create_dataset(opt)
     dataset_size = len(dataset)    # get the number of images in the dataset.
     print('The number of training images = %d' % dataset_size)
+    stats = TrainStats(opt)
 
     model = CycleGan(opt)
+    total_iters = 0                # the total number of training iterations
 
-    i = iter(dataset)
-    n = next(i)
+    for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):
+        epoch_start_time = time.time()
+        epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
+        model.update_learning_rate()
+        for i, data in enumerate(dataset):
+            model.feed_input(data)
+            iter_start_time = time.time()
+            model.optimize_parameters()
 
-    fig, ax = plt.subplots(nrows=1, ncols=2)
-    ax[0].imshow(n['A'][0].permute(1, 2, 0).numpy())
+            total_iters += 1
+            epoch_iter += 1
 
-    ax[1].imshow(n['B'][0].permute(1, 2, 0).numpy())
-    plt.show(block=True)
+            if total_iters % opt.print_freq == 0:
+                t_data = iter_start_time - epoch_start_time
+                t_comp = (time.time() - iter_start_time) / opt.batch_size
+                stats.print_current_losses(epoch, epoch_iter, model.get_current_losses(), t_comp, t_data)
+                stats.save_current_visuals(model.get_current_visuals(), 'img-%s' % total_iters)
+
+            if total_iters % opt.save_latest_freq == 0:
+                print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
+                save_suffix = 'iter_%d' % total_iters if opt.save_by_iter else 'latest'
+                model.save_networks(save_suffix)
+
+        if epoch % opt.save_epoch_freq == 0:
+            print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
+            model.save_networks('latest')
+            model.save_networks(str(epoch))
+
+        print('End of epoch %d / %d \t Time Taken: %d sec' %
+              (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
--- utils/TrainStats.py
+++ utils/TrainStats.py
@@ -1,5 +1,8 @@
 import os
 import time
+import torch
+import numpy as np
+from PIL import Image
 
 
 class TrainStats:
@@ -7,6 +10,7 @@ class TrainStats:
 
         # create a logging file to store training losses
         self.log_name = os.path.join(opt.checkpoints_dir, opt.name, 'loss_log.txt')
+        self.img_dir = os.path.join(opt.checkpoints_dir, opt.name, 'visuals')
         with open(self.log_name, "a") as log_file:
             now = time.strftime("%c")
             log_file.write('================ Training Loss (%s) ================\n' % now)
@@ -32,3 +36,56 @@ class TrainStats:
         print(message)  # print the message
         with open(self.log_name, "a") as log_file:
             log_file.write('%s\n' % message)
+
+    def save_current_visuals(self, images, prefix):
+        """Save Current Produced images
+
+        :param images: Images
+        :type images dict
+        :param prefix: Name prefix
+        :return:
+        """
+        for label, image in images.items():
+            imageName = '%s-%s.jpg' % (prefix, label)
+            imagePath = os.path.join(self.img_dir, imageName)
+            img = self.tensor2im(image)
+            self.save_image(img, imagePath)
+
+    def tensor2im(self, input_image, imtype=np.uint8):
+        """Converts a Tensor array into a numpy image array.
+
+        :param input_image: the input image tensor array
+        :param imtype: the desired type of the converted numpy array
+        :return: Converted Image
+        """
+        if not isinstance(input_image, np.ndarray):
+            if isinstance(input_image, torch.Tensor):  # get the data from a variable
+                image_tensor = input_image.data
+            else:
+                return input_image
+            image_numpy = image_tensor[0].cpu().float().numpy()  # convert it into a numpy array
+            if image_numpy.shape[0] == 1:  # grayscale to RGB
+                image_numpy = np.tile(image_numpy, (3, 1, 1))
+
+            # post-processing: transpose and scaling
+            image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
+        else:  # if it is a numpy array, do nothing
+            image_numpy = input_image
+        return image_numpy.astype(imtype)
+
+    def save_image(self, image_numpy, image_path, aspect_ratio=1.0):
+        """Save a numpy image to the disk
+
+        :param image_numpy: input numpy array
+        :param image_path: the path of the image
+        :param aspect_ratio: aspect_ratio of the image
+        """
+
+        image_pil = Image.fromarray(image_numpy)
+        h, w, _ = image_numpy.shape
+
+        if aspect_ratio > 1.0:
+            image_pil = image_pil.resize((h, int(w * aspect_ratio)), Image.BICUBIC)
+        if aspect_ratio < 1.0:
+            image_pil = image_pil.resize((int(h / aspect_ratio), w), Image.BICUBIC)
+        image_pil.save(image_path)

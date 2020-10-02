297c88018a8855690cda3b91c836f46a899d04be
Arkadip <in2arkadipb13@gmail.com>
2020-10-02 21:04:56 +0530

    feat: added some inference args

--- options/TrainOptions.py
+++ options/TrainOptions.py
@@ -8,11 +8,20 @@ class TrainOptions(BaseOptions):
 
     def initialized(self, parser):
         parser = BaseOptions.initialized(self, parser)
+
+        # Training Stats params
+        parser.add_argument('--print_freq', type=int, default=100,
+                            help='frequency of showing training results on console')
+
         # network saving and loading parameters
         parser.add_argument('--phase', type=str, default='train', help='train, val, test, etc')
         parser.add_argument('--epoch_count', type=int, default=1,
                             help='the starting epoch count, '
                                  'we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>,')
+        parser.add_argument('--save_latest_freq', type=int, default=5000, help='frequency of saving the latest results')
+        parser.add_argument('--save_epoch_freq', type=int, default=5,
+                            help='frequency of saving checkpoints at the end of epochs')
+        parser.add_argument('--save_by_iter', action='store_true', help='whether saves model by iteration')
 
         # Training Parameters
         parser.add_argument('--n_epochs', type=int, default=100, help='number of epochs with the initial learning rate')

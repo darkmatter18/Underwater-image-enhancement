213c2710740f02a4d6d7080ddf59752d2f57fd17
Arkadip <in2arkadipb13@gmail.com>
2020-10-17 09:14:25 +0530

    fix: Tensor casting to bool error CycleGan.py
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- model/CycleGan.py
+++ model/CycleGan.py
@@ -416,12 +416,12 @@ class CycleGan:
         """
         r = collections.OrderedDict({'real_A': self.real_A, 'real_B': self.real_B})
 
-        if self.fake_A:
+        if self.fake_A is not None:
             r['fake_A'] = self.fake_A
-        if self.fake_B:
+        if self.fake_B is not None:
             r['fake_B'] = self.fake_B
-        if self.rec_A:
+        if self.rec_A is not None:
             r['rec_A'] = self.rec_A
-        if self.rec_B:
+        if self.rec_B is not None:
             r['rec_B'] = self.rec_B
         return r

f0ef6d54903c007b413492b3c5e90dbc3127479c
Arkadip <in2arkadipb13@gmail.com>
2020-10-17 09:02:22 +0530

    function docs Added /data/__init__.py
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- data/__init__.py
+++ data/__init__.py
@@ -3,6 +3,11 @@ from .dataloaders import CustomDatasetDataLoader
 
 
 def create_dataset(opt):
+    """Create a Dataset using opt
+
+    :param opt: The options
+    :return: Loaded torch.tensor dataset
+    """
     dataset = CustomDataset(dataroot=opt.dataroot, phase=opt.phase, max_dataset_size=opt.max_dataset_size,
                             direction=opt.direction, input_nc=opt.input_nc, output_nc=opt.output_nc,
                             serial_batches=opt.serial_batches, preprocess=opt.preprocess, flip=not opt.no_flip,

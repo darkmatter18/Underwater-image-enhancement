f5b9625caf223b992b80801be6afe5bc50506c99
Arkadip <in2arkadipb13@gmail.com>
2021-06-19 17:14:13 +0530

    Save objects added
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- utils/SaveObjects.py
+++ utils/SaveObjects.py
@@ -1,24 +1,15 @@
+import os
 import torch
 
 
 class SaveObject:
     def __init__(self, opt):
         self.provider = opt.cloud
-        if self.provider == "aws":
-            # Do AWS related stuff
-            pass
-        elif self.provider == "gcp":
-            # Do GCP related stuff
-            pass
-        else:
-            # Do ofr local
-            pass
 
-    def save_torch_artifacts(self, artifact: dict, save_path: str) -> None:
-        torch.save(artifact, save_path)
-        if self.provider == "gcp":
-            # Do after saving works here
-            pass
+    def save_model(self, model, model_dir):
+        path = os.path.join(model_dir, "model.pth")
+        # recommended way from http://pytorch.org/docs/master/notes/serialization.html
+        torch.save(model.cpu().state_dict(), path)
 
     def save_file(self):
         pass

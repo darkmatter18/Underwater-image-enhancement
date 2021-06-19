66f9bd8d9f5cad0dfd79bb819343f16bac002ac6
Arkadip <in2arkadipb13@gmail.com>
2021-06-19 19:32:28 +0530

    Save provider added
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- utils/SaveObjects.py
+++ utils/SaveObjects.py
@@ -4,12 +4,16 @@ import torch
 
 class SaveObject:
     def __init__(self, opt):
+        self.opt = opt
         self.provider = opt.cloud
 
-    def save_model(self, model, model_dir):
-        path = os.path.join(model_dir, "model.pth")
-        # recommended way from http://pytorch.org/docs/master/notes/serialization.html
-        torch.save(model.cpu().state_dict(), path)
+    def save_model(self, model, file_name):
+        if self.provider == "aws":
+            path = os.path.join(self.opt.model_dir, file_name)
+            # recommended way from http://pytorch.org/docs/master/notes/serialization.html
+            torch.save(model.cpu().state_dict(), path)
+        else:
+            raise RuntimeError('Save provider can be only aws sagemaker')
 
     def save_file(self):
         pass

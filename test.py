2bd246dced2c9ac64718c9aa261b03d68ecabe5c
Arkadip <in2arkadipb13@gmail.com>
2021-06-20 10:53:46 +0530

    Train test file taking to ground
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- test.py
+++ test.py
@@ -16,7 +16,10 @@ def main():
     viz = TestVisualizer(opt)
 
     # Dataset
-    dataset = create_dataset(opt)
+    dataset = create_dataset(dataroot=opt.training_data_dir, serial_batches=opt.serial_batches,
+                             preprocess=opt.preprocess, no_flip=opt.no_flip, load_size=opt.load_size,
+                             crop_size=opt.crop_size, batch_size=opt.batch_size, num_threads=opt.num_threads,
+                             is_distributed=opt.is_distributed, use_cuda=opt.use_cuda)
 
     # setup Gan
     cycleGan = create_model(opt)

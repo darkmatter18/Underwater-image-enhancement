7523a0dd1768c0b5b02e03162bec6d80d09c90fe
Arkadip <in2arkadipb13@gmail.com>
2021-07-01 13:29:22 +0530

    setup cloud added term for colab
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- utils/setup_cloud.py
+++ utils/setup_cloud.py
@@ -1,4 +1,6 @@
 import os
+
+import torch.cuda
 import torch.distributed as dist
 
 
@@ -18,6 +20,15 @@ def setup_cloud(opt):
             dist.init_process_group(backend=opt.backend, rank=host_rank, world_size=world_size)
 
         opt.is_distributed = is_distributed
+        opt.use_cuda = use_cuda
+        return opt
+
+    if opt.cloud == "colab":
+        # logger.debug("Distributed training - {}".format(is_distributed))
+        use_cuda = opt.num_gpus > 0 and torch.cuda.is_available()
+        # logger.debug("Number of gpus available - {}".format(args.num_gpus))
+
+        opt.is_distributed = False
         opt.use_cuda = use_cuda
         return opt
 

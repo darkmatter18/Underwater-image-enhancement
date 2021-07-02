8aea92a4b2d9fe5e40baea202536021a0e2b59a6
Arkadip <in2arkadipb13@gmail.com>
2021-07-02 13:28:59 +0530

    SaveObjects.py added entries for colab
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- utils/SaveObjects.py
+++ utils/SaveObjects.py
@@ -8,20 +8,20 @@ class SaveObject:
         self.provider = opt.cloud
 
     def save_model(self, model, file_name):
-        if self.provider == "aws":
+        if self.provider == "aws" or self.provider == "colab":
             path = os.path.join(self.opt.model_dir, file_name)
             # recommended way from http://pytorch.org/docs/master/notes/serialization.html
             torch.save(model.cpu().state_dict(), path)
         else:
-            raise RuntimeError('Save provider can be only aws sagemaker')
+            raise RuntimeError('Save provider can be only aws sagemaker and colab')
 
     def save_optim_scheduler(self, optim_or_s, name):
-        if self.provider == "aws":
+        if self.provider == "aws" or self.provider == "colab":
             path = os.path.join(self.opt.model_dir, name)
             # recommended way from http://pytorch.org/docs/master/notes/serialization.html
             torch.save(optim_or_s.state_dict(), path)
         else:
-            raise RuntimeError('Save provider can be only aws sagemaker')
+            raise RuntimeError('Save provider can be only aws sagemaker and colab')
 
     def save_file(self):
         pass

89218a198d288584a363cc7b9db050c2e13a795a
Arkadip <in2arkadipb13@gmail.com>
2021-07-09 18:47:41 +0530

    Added save artifacts augements
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- utils/TestVisualizer.py
+++ utils/TestVisualizer.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from skimage.measure import shannon_entropy
 from skimage.metrics import peak_signal_noise_ratio, structural_similarity
 
-from . import tensor2im
+from . import tensor2im, mkdirs
 from .nmetrics import nmetrics
 
 
@@ -17,6 +17,8 @@ class TestVisualizer:
         self.subdir = opt.test_subdir
         self.phase = opt.phase
         self.visuals = opt.visuals
+        self.save_artifacts = opt.save_artifacts
+        self.all = opt.all
 
         self.dir_A = os.path.join(self.dataroot, self.subdir, self.phase + 'A')  # create a path '/path/to/data/trainA'
         self.dir_B = os.path.join(self.dataroot, self.subdir, self.phase + 'B')  # create a path '/path/to/data/trainB'
@@ -52,8 +54,24 @@ class TestVisualizer:
 
             plt.show()
 
-        with open(os.path.join(os.getcwd(), "output", f"{self.opt.load_model}_data.pkl"), 'wb+') as f:
-            pickle.dump({'psnr': self.psrns, 'ssim': self.ssims}, f)
+        if self.save_artifacts:
+            p = os.path.join(os.getcwd(), "output", "images")
+            mkdirs(p)
+            for i, (r_i, f_i, o_f_i) in enumerate(zip(
+                    self.real_images, self.fake_images, self.original_of_fake_images)):
+                mpimg.imsave(os.path.join(p, f"real_A_{self.opt.load_model}_{i}.jpg"), r_i)
+                mpimg.imsave(os.path.join(p, f"fake_A_{self.opt.load_model}_{i}.jpg"), f_i)
+                mpimg.imsave(os.path.join(p, f"real_B_{self.opt.load_model}_{i}.jpg"), o_f_i)
+            with open(os.path.join(p, f"{self.opt.load_model}_data.pkl"), 'wb+') as f:
+                pickle.dump({'psnr': self.psrns, 'ssim': self.ssims, 'entropy': self.entropy, 'uiqm': self.uiqms,
+                             'uciqm': self.uciqes}, f)
+
+        if self.all:
+            p = os.path.join(os.getcwd(), "output", "metrics")
+            mkdirs(p)
+            with open(os.path.join(p, f"{self.opt.load_model}_data.pkl"), 'wb+') as f:
+                pickle.dump({'psnr': self.psrns, 'ssim': self.ssims, 'entropy': self.entropy, 'uiqm': self.uiqms,
+                             'uciqm': self.uciqes}, f)
 
     def add_inference(self, image_data: dict, image_path: dict):
         """Displays the test image data
@@ -79,10 +97,9 @@ class TestVisualizer:
                     entropy = shannon_entropy(fake_i)
                     uiqm, uciqe = nmetrics(fake_i)
 
-                if self.visuals:
-                    self.real_images.append(real_i)
-                    self.fake_images.append(fake_i)
-                    self.original_of_fake_images.append(original_of_fake_i)
+                self.real_images.append(real_i)
+                self.fake_images.append(fake_i)
+                self.original_of_fake_images.append(original_of_fake_i)
                 self.path_names.append(os.path.basename(os.path.normpath(image_path["a"][0])))
                 self.psrns.append(psnr)
                 self.ssims.append(ssim)
